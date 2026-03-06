f4a442b5ef79dd79eb830789f9fcd0bdd61b7725
Jan Michael Martirez <jmmartirez@gmail.com>
2026-03-07 06:33:30 +0800

    fix: add fastify/env

--- src/server.tsx
+++ src/server.tsx
@@ -1,4 +1,6 @@
 import Fastify from "fastify";
+import fastifyEnv from "@fastify/env";
+import config from "./config/config.json"
 import fastifyPrintRoutes from 'fastify-print-routes'
 import rootRoutes from "./routes/root.routes";
 import userRoutes from "./routes/user.routes";
@@ -7,17 +9,31 @@ const app = Fastify({
   logger: true,
 });
 
+const options = {
+  confKey: 'config', 
+  schema: config
+}
+
 app.register(fastifyPrintRoutes);
 app.register(rootRoutes);
 app.register(userRoutes, { prefix: "/api/v1/users" });
+await app.register(fastifyEnv, options).ready((err) => {
+  if (err) console.error(err);
 
-app.listen({
-  port: 3000
-}, (err, address) => {
-  app.log.debug("Hello World");
+  console.log(app.config);
+  console.log(app.getEnvs());
+});
 
-  if(err) {
-    app.log.error(err);
-    process.exit(1);
-  }
+app.after(() => {
+  app.listen({
+    port: app.config.PORT
+  }, (err, address) => {
+    app.log.debug("Hello World", app.config);
+
+    if(err) {
+      console.log(app.config.PORT);
+      app.log.error(err);
+      process.exit(1);
+    }
+  })
 })
